242db16149b974fd7e641397a38ff3aa999a88d1
daniel.chiou <ilu73224@gmail.com>
2015-06-02 15:08:01 +0800

    add shared preference to remember original music volume.

--- app/src/main/java/com/stc/audiogood/MainActivity.java
+++ app/src/main/java/com/stc/audiogood/MainActivity.java
@@ -2,6 +2,7 @@ package com.stc.audiogood;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -11,11 +12,14 @@ import java.io.IOException;
 
 public class MainActivity extends Activity {
     // from 0 to 15
-    private static final int VALUE_MUSIC_VOLUME = 10;
+    private static final int DEFAULT_VALUE_MUSIC_VOLUME = 10;
 
     private static final String ZEN_MODE = "zen_mode";
     private static final int ZEN_MODE_OFF = 0;
     private static final int ZEN_MODE_NO_INTERRUPTIONS = 2;
+
+    public static final String PREFS_NAME = "AudioGoodPref";
+    public static final String KEY_MUSIC_VOLUME = "AudioGoodPref";
 
     private AudioManager audio=null;
     private Process p=null;
@@ -35,12 +39,19 @@ public class MainActivity extends Activity {
 
         int value = Settings.Global.getInt(getApplicationContext().getContentResolver(), ZEN_MODE, ZEN_MODE_OFF);
 
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+
         if(ZEN_MODE_OFF == value) {
             Settings.Global.putInt(getApplicationContext().getContentResolver(), ZEN_MODE, ZEN_MODE_NO_INTERRUPTIONS);
+            int music_volume = audio.getStreamVolume(AudioManager.STREAM_MUSIC);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putInt(KEY_MUSIC_VOLUME, music_volume);
+            editor.commit();
             audio.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0);
         }else{
             Settings.Global.putInt(getApplicationContext().getContentResolver(), ZEN_MODE, ZEN_MODE_OFF);
-            audio.setStreamVolume(AudioManager.STREAM_MUSIC, VALUE_MUSIC_VOLUME, 0);
+            int music_volume = settings.getInt(KEY_MUSIC_VOLUME, DEFAULT_VALUE_MUSIC_VOLUME);
+            audio.setStreamVolume(AudioManager.STREAM_MUSIC, music_volume, 0);
         }
         finish();
     }
